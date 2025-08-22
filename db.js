b088c180f0b83c529146a203ad169a51c095bd7c
mukonanecuvy <mukonanekhubvi@unitacsolution.co.za>
2025-08-22 14:13:03 +0200

    Added env file

--- db.js
+++ db.js
@@ -1,14 +1,13 @@
-const mysql = require('mysql2/promise')
+import dotenv from "dotenv";
+import mysql from "mysql2/promise";
+
+dotenv.config();
 
 const mysqlPool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
 
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'employee_dp'
-}
-
-)
-
-
-module.exports = mysqlPool;
+export default mysqlPool;
